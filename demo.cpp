03c1af3258d6d31c1bfa38028d8d36150ded193d
Anurag0Verma <211006@kit.ac.in>
2022-09-28 23:42:03 +0530

    in desktop

--- demo.cpp
+++ demo.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-//#include <process.h>
+#include <process.h>
 using namespace std;
 
 int main()
 {
- //   system("cls");
-   // system("color a");
+    system("cls");
+    system("color a");
+    string s;
+    cin >> s;
+    // int num1, num2;
+    // cin >> num1 >> num2;
+    // int sum = num1 + num2;
+    // cout << sum << endl;
 
-    int num1, num2;
-    cin >> num1 >> num2;
-    int sum = num1 + num2;
-    cout << sum << endl;
+    cout << s;
 
     return 0;
 }
